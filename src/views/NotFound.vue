32d6c9fdc97f4dee88d33ed82be473764ef7838c
HTran21 <tranhoangtran22225@gmail.com>
2023-10-09 15:29:34 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="page">
+        <p>
+            Opp, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
