7b814251372208bf618c64a2d339c073532bbd15
HTran21 <tranhoangtran22225@gmail.com>
2023-10-09 16:20:33 +0700

    Tao trang cap nhat lien he

--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -13,6 +13,9 @@ class ConactService {
     async deleteAll() {
         return (await this.api.delete("/")).data;
     }
+    async get(id) {
+        return (await this.api.get(`/${id}`)).data;
+    }
     async update(id, data) {
         return (await this.api.put(`/${id}`, data)).data;
     }
